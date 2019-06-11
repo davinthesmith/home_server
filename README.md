76bff2863ad8c4db335bd2fccc550fcb45fe98d6
Davin Smith <davinsmith@outlook.com>
2019-06-11 18:33:34 -0500

    Added prod/dev deployment support

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ An in-home server used for toomanyprojects
 
 ## Setup
 Run `yarn setup` to initialize the project
+
+## Debug
+In the root directory, run `yarn dev`. This will start all of the docker containers in a debug state.  Then, whichever application you wish to debug, navigate to that folder and run `yarn watch`
--- scripts/init.sh
+++ scripts/init.sh
@@ -4,5 +4,10 @@ GRAPHQL_FOLDER="$PWD/graphql"
 
 # Copy Mongo env file if it doesn't exist
 if [ ! -f ${SCRIPTS_FOLDER}/../mongo.env ]; then 
-  cp ${SCRIPTS_FOLDER}/docker/sample.mongo.env ${SCRIPTS_FOLDER}/../mongo.env
+  cp ${SCRIPTS_FOLDER}/docker/db/sample.mongo.env ${SCRIPTS_FOLDER}/../mongo.env
+fi
+
+# Copy Graphql Dockerfile if it doesn't exist
+if [ ! -f ${GRAPHQL_FOLDER}/Dockerfile ]; then 
+  cp ${SCRIPTS_FOLDER}/docker/graphql/Dockerfile.dev ${GRAPHQL_FOLDER}/Dockerfile
 fi
