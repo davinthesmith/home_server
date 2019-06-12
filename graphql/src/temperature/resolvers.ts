cb7b8a705b297867c42842e4e6b1cf3e538affaf
Davin Smith <davinsmith@outlook.com>
2019-06-12 00:48:11 -0500

    Added query support

--- graphql/src/temperature/resolvers.ts
+++ graphql/src/temperature/resolvers.ts
@@ -14,16 +14,31 @@ interface MutationAddTemperatureArgs {
 
 export const resolvers = {
   Query: {
-    temperatures: async (parent: any, args: QueryTemperaturesArgs, ctx: any) : Promise<Temperature[]> => {
-      const db = await mongo();
-      return await db.collection(TEMPERATURE_COLLECTION).find({}).toArray();
+    temperatures: async (parent: any, args: QueryTemperaturesArgs, ctx: any): Promise<Temperature[]> => {
+      try {
+        const db = await mongo();
+
+        // build query object
+        let query: any = {};
+        if (args.source) query.source = args.source;
+
+        // build dateRange query
+        let dateQuery: any = {};
+        if (args.startDate) dateQuery["$gte"] = args.startDate;
+        if (args.endDate) dateQuery["$lte"] = args.endDate;
+        if (Object.keys(dateQuery).length) query.dateTime = { ...dateQuery };
+
+        return await db.collection(TEMPERATURE_COLLECTION).find(query).toArray();
+      } catch (err) { throw err }
     }
   },
   Mutation: {
-    addTemperature: async (parent: any, args: MutationAddTemperatureArgs, ctx: any) : Promise<Temperature> => {
-      const db = await mongo();
-      const result = await db.collection(TEMPERATURE_COLLECTION).insertOne(args.input);
-      return result.ops[0];
+    addTemperature: async (parent: any, args: MutationAddTemperatureArgs, ctx: any): Promise<Temperature> => {
+      try {
+        const db = await mongo();
+        const result = await db.collection(TEMPERATURE_COLLECTION).insertOne(args.input);
+        return result.ops[0];
+      } catch (err) { throw err }
     }
   }
 };
