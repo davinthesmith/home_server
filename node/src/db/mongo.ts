aa5b1efc9f48e0f0bcafdc34e8c58781dcd34c14
Davin Smith <davinsmith@outlook.com>
2019-06-15 14:45:11 -0500

    Add logging (#2)
    * change graphql to node
    
    * updated gitignore
    
    * change graphql to node
    
    * Added Express
    
    * Added debugger and logging middleware
    
    * Cleaned up winston logger

--- node/src/db/mongo.ts
+++ node/src/db/mongo.ts
@@ -1,16 +1,11 @@
 import { MongoClient, Db } from 'mongodb';
-
-const MONGO_DB = process.env.MONGO_DB;
-const MONGO_URL = process.env.MONGO_URL;
-const USERNAME = process.env.MONGO_DB_ROOT_USERNAME;
-const PASSWORD = process.env.MONGO_DB_ROOT_PASSWORD;
-const MONGO_CONNSTRING = `mongodb://${USERNAME}:${PASSWORD}@${MONGO_URL}:27017/admin`;
+import { MONGO_CONNSTRING, MONGO_DB } from '../constants'
 
 let db: Db;
 export const mongo = async () => {
   if (db) return db;
   try {
-    const client = await MongoClient.connect(MONGO_CONNSTRING, { poolSize: 5 });
+    const client = await MongoClient.connect(MONGO_CONNSTRING, { poolSize: 5, useNewUrlParser: true });
     db = client.db(MONGO_DB);
     return db;
   } catch (err) {
--- node/src/index.ts
+++ node/src/index.ts
@@ -1,12 +1,29 @@
-import { ApolloServer } from 'apollo-server';
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import { log } from './utils/logger'
 import resolvers from './root/resolvers'
 import typeDefs from './root/typeDefs'
+import { APP_URL, APP_PORT } from './constants';
 
+// setup Express
+const app = express();
+
+// setup ApolloServer and add to Express
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  formatError: error => {
+    log.error(error);
+    return error;
+  },
+  formatResponse: (response: any) => {
+    log.info(response);
+    return response;
+  },
 });
+server.applyMiddleware({ app });
 
-server.listen().then(({ url }: { url: string }) => {
-  console.log(`🚀  Server ready at ${url}`);
+// start Express
+app.listen({ port: APP_PORT }, () => {
+  console.log(`🚀 Server ready at ${APP_URL}:${APP_PORT}${server.graphqlPath}`);
 });
