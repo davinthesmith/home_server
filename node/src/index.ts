579c1df0fcbbdbe036a9565c751bf3f234f8f2c1
Davin Smith <davinsmith@outlook.com>
2019-06-15 11:27:59 -0500

    Added Express

--- node/src/index.ts
+++ node/src/index.ts
@@ -1,12 +1,19 @@
-import { ApolloServer } from 'apollo-server';
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
 import resolvers from './root/resolvers'
 import typeDefs from './root/typeDefs'
+
+const APP_URL = process.env.APP_URL;
+const APP_PORT = process.env.APP_PORT;
 
 const server = new ApolloServer({
   typeDefs,
   resolvers
 });
 
-server.listen().then(({ url }: { url: string }) => {
-  console.log(`🚀  Server ready at ${url}`);
+const app = express();
+server.applyMiddleware({ app });
+
+app.listen({ port: APP_PORT }, () => {
+  console.log(`🚀 Server ready at ${APP_URL}:${APP_PORT}${server.graphqlPath}`)
 });
