8b52291084831bbf1623d7b669f735a0acdac106
Davin Smith <davinsmith@outlook.com>
2019-06-16 07:16:55 -0500

    Update dev dependencies

--- scripts/init_dev.sh
+++ scripts/init_dev.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
+ROOT_FOLDER="$PWD"
 SCRIPTS_FOLDER="$PWD/scripts"
 NODE_FOLDER="$PWD/node"
 
 # MongoDB Files
-cp ${SCRIPTS_FOLDER}/env/db/dev.env ${SCRIPTS_FOLDER}/../mongo.env
+cp ${SCRIPTS_FOLDER}/env/db/dev.env ${ROOT_FOLDER}/mongo.env
 
 # Node Files
 cp ${SCRIPTS_FOLDER}/env/node/dev.env ${NODE_FOLDER}/.env
 cp ${SCRIPTS_FOLDER}/docker/node/Dockerfile.dev ${NODE_FOLDER}/Dockerfile
+
+# Docker Compose
+cp ${SCRIPTS_FOLDER}/docker/dev.docker-compose.yml ${ROOT_FOLDER}/docker-compose.yml
