fb2ebaf6bee9d34f2f40a7583eae7269694163a9
Davin Smith <davinsmith@outlook.com>
2019-06-16 11:08:56 -0500

    Prepare for first deploy (#4)
    * Update dev dependencies
    
    * Cleaned up dev some more
    
    * Persisted data for mongo

--- scripts/init_dev.sh
+++ scripts/init_dev.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
+ROOT_FOLDER="$PWD"
+MONGO_FOLDER="$PWD/mongo"
 SCRIPTS_FOLDER="$PWD/scripts"
 NODE_FOLDER="$PWD/node"
 
 # MongoDB Files
-cp ${SCRIPTS_FOLDER}/env/db/dev.env ${SCRIPTS_FOLDER}/../mongo.env
+mkdir -p ${MONGO_FOLDER}
+cp ${SCRIPTS_FOLDER}/env/db/dev.env ${MONGO_FOLDER}/.env
 
 # Node Files
 cp ${SCRIPTS_FOLDER}/env/node/dev.env ${NODE_FOLDER}/.env
 cp ${SCRIPTS_FOLDER}/docker/node/Dockerfile.dev ${NODE_FOLDER}/Dockerfile
+
+# Docker Compose
+cp ${SCRIPTS_FOLDER}/docker/dev.docker-compose.yml ${ROOT_FOLDER}/docker-compose.yml
